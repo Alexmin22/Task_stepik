4ce92b9b9824849e95d302dd6c4334e6150fb455
Alexmin22 <yashi80@list.ru>
2023-01-30 17:19:26 +0300

    не работает

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader isr = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            if (isr.read() != -1) {
+                int inter = isr.read();
+                int sec = 0;
+                if (inter == 13) {
+                    sec = inter;
+                } else if (inter != 10 && sec != 13) {
+                    System.out.println(inter);
+                }
+            } else if (isr.read() == -1) {
+                System.out.flush();
+                break;
+            }
+        }
     }
 }
+
